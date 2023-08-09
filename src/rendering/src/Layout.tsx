e6912d72f5c7d3c884043f83b181dfe25d4dc4b8
Nam Thanh Ho <nho@hugeinc.com>
2023-08-09 16:35:30 +0700

    testing moosend/sitecore send

--- src/rendering/src/Layout.tsx
+++ src/rendering/src/Layout.tsx
@@ -74,6 +74,13 @@ const Layout = ({ layoutData, headLinks }: LayoutProps): JSX.Element => {
           <link rel={headLink.rel} key={headLink.href} href={headLink.href} />
         ))}
         <meta name="robots" content="noindex" />
+        <script>
+          //load TrackerJS
+          !function(t,n,e,o,a){function d(t){var n=~~(Date.now()/3e5),o=document.createElement(e);o.async=!0,o.src=t+"?ts="+n;var a=document.getElementsByTagName(e)[0];a.parentNode.insertBefore(o,a)}t.MooTrackerObject=a,t[a]=t[a]||function(){return t[a].q?void t[a].q.push(arguments):void(t[a].q=[arguments])},window.attachEvent?window.attachEvent("onload",d.bind(this,o)):window.addEventListener("load",d.bind(this,o),!1)}(window,document,"script","//cdn.stat-track.com/statics/moosend-tracking.min.js","mootrack");
+          //tracker has to be initialized otherwise it will generate warnings and wont sendtracking events
+          mootrack('init', '7148962a-efec-4734-9522-d695744e9b20');
+          mootrack('trackPageView');
+        </script>
       </Head>
 
       {/* root placeholder for the app, which we add components to using route data */}
